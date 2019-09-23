6303be4f9f5fe5c196ad6820edeb8d799db2fefc
hieuphan25 <mhieu25101998@gmail.com>
2019-09-23 14:47:25 +0700

    add teacher search

--- ELearning/app/Http/Controllers/TeacherController.php
+++ ELearning/app/Http/Controllers/TeacherController.php
@@ -16,11 +16,20 @@ class TeacherController extends Controller
         $this->service = $teacherService;
     }
 
+    public function index(Request $request)
+    {
+        return $this->service->index($request);
+    }
+
     public function register(RegisterTeacherRequest $request)
     {
         return $this->service->register($request);
     }
 
+    public function detail($id)
+    {
+        return $this->service->detail($id);
+    }
     public function info()
     {
         return $this->service->info();
