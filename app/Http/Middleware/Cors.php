d3279aee5a3cd3c2465e83a1f3a8b20693c83c72
hieuphan25 <mhieu25101998@gmail.com>
2019-10-04 02:02:50 +0700

    update

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -19,6 +19,7 @@ class Cors
             ->header('Access-Control-Allow-Headers', '*')
             ->header('Access-Control-Allow-Origin', '*')
             ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
-            ->header('Content-Type', 'application/json');
+            ->header('Content-Type', 'application/json')
+            ->header('Accept', 'application/json');
     }
 }
